df15defb023a540a7278cac07b29a068675e6b92
Jesus Rodolfo Gil farias <131034722+Jrgil20ucab@users.noreply.github.com>
2023-11-08 04:17:58 +0000

    demostracion de un array

--- demo/src/main/java/inicial/com/Main.java
+++ demo/src/main/java/inicial/com/Main.java
@@ -7,8 +7,10 @@ public class Main {
     private static final Logger logger = Logger.getLogger(Main.class.getName());
 
     public static void main(String[] args) {
+        
         // ejemplo de uso de logger.info() en lugar de System.out.println
         logger.info("Solo un ejemplo en java");
+
         //prueba de concatenacion de tipos de datos
         byte zero = 0;
         short a = 3;
@@ -18,5 +20,20 @@ public class Main {
         boolean f = true;
         String output = "H" + a + b + b + zero + d + "w" + zero + "r" + b + "d" + d + e + d + f;
         logger.info(output);
+
+        //prueba de uso de array 
+        int[] miArray = new int[5]; // Crea un array de enteros con 5 elementos
+
+        // Asigna valores a los elementos del array
+        miArray[0] = 10;
+        miArray[1] = 20;
+        miArray[2] = 30;
+        miArray[3] = 40;
+        miArray[4] = 50;
+
+        // Imprime los elementos del array
+        for (int i = 0; i < miArray.length; i++) {
+            logger.info(String.valueOf(miArray[i]));
+        }
     }
 }
