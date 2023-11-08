d1f0b60c27c0c8987e97a21f42c0b27d8e58d006
Jesus Rodolfo Gil farias <131034722+Jrgil20ucab@users.noreply.github.com>
2023-11-08 03:48:56 +0000

    cambio de systen a logger

--- demo/src/main/java/inicial/com/Main.java
+++ demo/src/main/java/inicial/com/Main.java
@@ -1,8 +1,19 @@
 package inicial.com;
 
+import java.util.logging.Logger;
+
 public class Main {
+    private static final Logger logger = Logger.getLogger(Main.class.getName());
+
     public static void main(String[] args) {
-        System.out.println("Solo un ejemplo en java");
-        
+        logger.info("Solo un ejemplo en java");
+        byte zero = 0;
+        short a = 3;
+        int b = 1;
+        char d = ' ';
+        float e = 2.0f;
+        boolean f = true;
+        String output = "H" + a + b + b + zero + d + "w" + zero + "r" + b + "d" + d + e + d + f;
+        logger.info(output);
     }
 }
