8d21e55e9c3376b97d70df62aa989b8ef4532fe7
Jesus Rodolfo Gil farias <131034722+Jrgil20ucab@users.noreply.github.com>
2023-11-08 04:04:04 +0000

    comentarios al respecto de los cambios

--- demo/src/main/java/inicial/com/Main.java
+++ demo/src/main/java/inicial/com/Main.java
@@ -1,12 +1,15 @@
 package inicial.com;
 
+//se importa la clase Logger para evitar el uso de System.out.println por la regla de sonar Java:S106
 import java.util.logging.Logger;
 
 public class Main {
     private static final Logger logger = Logger.getLogger(Main.class.getName());
 
     public static void main(String[] args) {
+        // ejemplo de uso de logger.info() en lugar de System.out.println
         logger.info("Solo un ejemplo en java");
+        //prueba de concatenacion de tipos de datos
         byte zero = 0;
         short a = 3;
         int b = 1;
